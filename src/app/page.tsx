ae99bdd033c3a527e6bf71fab914767c60be18c7
Adam <adamdobbins@hotmail.com>
2025-03-29 18:34:50 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <header className="py-8 text-center">
         <h1 className="text-5xl font-bold mb-2">Adam&apos;s Portfolio</h1>
         <p className="text-xl">
-          Bla | Bla | Bla
+          Bla | BlaAAA | Bla
         </p>
       </header>
 
